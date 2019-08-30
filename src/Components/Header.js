0698d11f9b97a4b00a7a43eac55a30805296f3bc
marco-yc-song <marco@peopet.co.kr>
2019-08-30 16:29:57 +0900

    style과 라우터를 활용한 네비게이션 구현

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -18,13 +18,21 @@ const Header = styled.header`
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
+const List = styled.ul`
+  display: flex;
+`;
+
 const Item = styled.li`
   width: 50px;
   height: 50px;
   text-align: center;
+  margin-right: 3px;
   /* &:not(:last-child) {
     margin-right: 10px;
   } */
+
+  border-bottom: 5px solid ${props => (props.current ? "red" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -34,22 +42,38 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-const List = styled.ul`
-  display: flex;
-`;
-
-export default () => (
+const HeaderComponent = ({ location: { pathname } }) => (
   <Header>
+    {console.log(pathname)}
     <List>
-      <Item>
+      <Item current={pathname === "/"}>
         <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/TV"}>
         <SLink to="/TV">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/Search"}>
         <SLink to="/Search">Search</SLink>
       </Item>
     </List>
   </Header>
 );
+
+// function HeaderComponent(props) {
+//   <Header>
+//     {console.log(props)}
+//     <List>
+//       <Item current={false}>
+//         <SLink to="/">Movies</SLink>
+//       </Item>
+//       <Item current={true}>
+//         <SLink to="/TV">TV</SLink>
+//       </Item>
+//       <Item current={false}>
+//         <SLink to="/Search">Search</SLink>
+//       </Item>
+//     </List>
+//   </Header>
+// }
+
+export default withRouter(HeaderComponent);
