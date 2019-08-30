c3ae6145a743b3a006276af0676799b87b3b4f3c
marco-yc-song <marco@peopet.co.kr>
2019-08-30 15:29:16 +0900

    3.3 스타일링 실습

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,18 +8,23 @@ import {
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 //exact 들어가면 반드시 일치해야됨
 //Switch : 한번에 한 놈만 라우팅 시켜줌
 //Redirect : 특정 패스를 다른 패스로 리디렉션 시켜줌(지금은 나머지 주소를 전부 루트로보냄)
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/TV" exact component={TV} />
-      <Route path="/TV/popular" render={() => <h1>popular!</h1>} />
-      <Route path="/Search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/TV" exact component={TV} />
+        <Route path="/TV/popular" render={() => <h1>popular!</h1>} />
+        <Route path="/Search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
